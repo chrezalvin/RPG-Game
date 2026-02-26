d52982783f59dc4306ab2817b926dfc5ee9e8d72
chrezalvin <chrezalvin@gmail.com>
2026-02-27 00:47:29 +0700

    changed structure of settings for audio savefile

--- src/Prefabs/Menu/UserSettingsMenu.rb
+++ src/Prefabs/Menu/UserSettingsMenu.rb
@@ -14,15 +14,15 @@ class UserSettingsMenu < Menu
     def reload_menu(game)
         @menu_list = []
         @menu_list.push(MenuElement.new(
-            menu_name: "Use Volume       <#{game.is_use_audio ? "On" : "Off"}>",
+            menu_name: "Use Volume       < #{game.is_use_audio ? "On" : "Off"} >",
             on_select_right: lambda {game.set_use_audio(!game.is_use_audio); reload_menu(game);},
             on_select_left: lambda {game.set_use_audio(!game.is_use_audio); reload_menu(game);},
             tooltip: "Press Left or Right arrow key to toggle"
         ))
         @menu_list.push(MenuElement.new(
-            menu_name: "Volume           (#{(game.volume * 100).to_i}%)" ,
-            on_select_right: lambda {game.set_audio([game.volume + 0.1, 1].min); reload_menu(game);},
-            on_select_left: lambda {game.set_audio([game.volume - 0.1, 0].max); reload_menu(game);},
+            menu_name: "Volume           < #{game.volume}% >" ,
+            on_select_right: lambda {game.set_audio([game.volume + 10, 100].min); reload_menu(game);},
+            on_select_left: lambda {game.set_audio([game.volume - 10, 0].max); reload_menu(game);},
             tooltip: "Press Left or Right arrow key to adjust volume"
         ))
         @menu_list.push(MenuElement.new(menu_name: "Back", on_selected: lambda {game.back_to_main_menu}))
